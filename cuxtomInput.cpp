b8554e38fd680d75bd76295168761feb2cc230e7
sakibahammed <sakib.ahammed2345@gmail.com>
2023-09-20 12:52:19 +0600

    if else in cpp meusm pices of shitt

---
+++ cuxtomInput.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number;
+    cout<<"Enter a Number\n";
+    cin>>number;
+
+    if(number>0){
+        cout<<"The number is positeve"<<endl;
+
+    }
+    else if(number ==0){
+        cout<<"The number is Zero"<<endl;
+
+    }
+
+    else{
+        cout<<"The number is negative surely";
+    }
+
+
+}
