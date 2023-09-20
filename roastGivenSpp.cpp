cc52e8f56a414aef7a766ad57927ab691d2ae6c9
sakibahammed <sakib.ahammed2345@gmail.com>
2023-09-20 14:08:15 +0600

    a buggy slaggy code

---
+++ roastGivenSpp.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void roastGiven(int personNumber){
+
+    // int roastNumber = roastNumber;
+    while(personNumber<7){
+        cout<<"Roast dewa cholbe"<<endl;
+    }
+    
+}
+
+
+int main(){
+    roastGiven(7);
+}
