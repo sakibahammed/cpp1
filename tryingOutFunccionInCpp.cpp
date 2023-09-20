c76fd4c9504f8cbe5acc514a40dfce533b918b80
sakibahammed <sakib.ahammed2345@gmail.com>
2023-09-20 13:10:33 +0600

    trying out creppy function in cpp

---
+++ tryingOutFunccionInCpp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+// Function declaration
+// int add(int a, int b) {
+//     return a + b;
+// }
+
+int add(int a,int b){
+    return a+b;
+}
+
+int main() {
+    int x = 12,y = 12;
+    double result = add(x,y);
+    cout<<result<<endl;
+} 
+
