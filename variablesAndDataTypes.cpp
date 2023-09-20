008e0fefff760d371e0b76f118c6b014389f649c
sakibahammed <sakib.ahammed2345@gmail.com>
2023-09-20 12:39:09 +0600

    all

---
+++ variablesAndDataTypes.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main(){
+    // declaring a variable in integer as a number because this museum pices of shiit doest understand what im doing;
+     int age = 20;
+     cout<<age<<endl;
+     
+
+     // declaring a float value with float . so that it can recognise what is going on
+
+
+     float pi = 3.12;
+     cout<<pi<<endl;
+
+     // declaring a value in double variable in cpp
+
+     double aRadius = 2.2342525689829;
+
+     cout<<aRadius<<endl;
+
+
+     bool isALive = false;
+
+     cout<<isALive<<endl;
+
+     // charValue in cpp
+
+    char gradee = 'A';
+    cout<<gradee<<endl;
+
+}
+
+
+
+
