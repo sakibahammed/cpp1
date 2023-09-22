a075b1cc142a418ff386c5200bef49088580c7c2
sakibahammed <sakib.ahammed2345@gmail.com>
2023-09-23 02:06:52 +0600

    i dnt know what i am reading

--- labsMAnual/dynamicMemoryallocatiion.cpp
+++ labsMAnual/dynamicMemoryallocatiion.cpp
@@ -18,29 +18,72 @@
 // dynamic memory allocation using array
 
 
+// #include<iostream>
+// using namespace std;
+
+
+// int main(){
+//     int  * dynamicArray = new int[4];
+//     for (int i =0;i<4;i++){
+//         dynamicArray[i] = i*10;
+//     }
+
+
+//     cout<<"Dynamic Array : ";
+//     for(int i=0;i<4;i++){
+//         cout<<dynamicArray[i]<<endl;
+//     }
+
+
+
+//     delete[] dynamicArray;
+
+
+
+
+// }
+
+
+// 2d dynamic array
+
 #include<iostream>
 using namespace std;
-
-
 int main(){
-    int  * dynamicArray = new int[4];
-    for (int i =0;i<4;i++){
-        dynamicArray[i] = i*10;
+    int numRows , numCols;
+
+
+    cout<<"enter the number of rows";
+
+
+    cin>>numRows;
+
+    cout<<"enter the number of cols";
+    cin>>numCols;
+
+    char **charArray = new char*[numRows]; // dynamic allocate a 2d array;
+
+    for(int i=0;i<numRows;++i){
+        charArray[i] = new char[numCols];
+
+    }
+
+    // input strings
+
+
+    cout<<"enter strings"<<"per line"<<endl;
+    for(int i=0;i<numRows;++i){
+        cin>>charArray[i];
     }
 
 
-    cout<<"Dynamic Array : ";
-    for(int i=0;i<4;i++){
-        cout<<dynamicArray[i]<<endl;
+    cout<<"entered string"<<endl;
+
+    for(int i=0;i<numRows;++i){
+        cout<<charArray[i]<<endl;
     }
 
 
-
-    delete[] dynamicArray;
-
-    
-
-
+    delete [] charArray;
 }
 
 
