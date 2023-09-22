0238284f588efb08cd063d7464fa9f22fb38c352
sakibahammed <sakib.ahammed2345@gmail.com>
2023-09-23 01:02:18 +0600

    array dynamic array

--- labsMAnual/dynamicMemoryallocatiion.cpp
+++ labsMAnual/dynamicMemoryallocatiion.cpp
@@ -1,14 +1,46 @@
+// #include<iostream>
+// using namespace std;
+
+
+// int main(){
+//     int * dynamicInt = new int;// Alllocating memory for and integer
+//     * dynamicInt = 14; // assignning the value
+
+
+//     cout<<"New dynamic int : "<< *dynamicInt<<endl;
+
+//     delete dynamicInt; // deallocating the memory
+//     return 0;
+// }
+
+
+
+// dynamic memory allocation using array
+
+
 #include<iostream>
 using namespace std;
 
 
 int main(){
-    int * dynamicInt = new int;// Alllocating memory for and integer
-    * dynamicInt = 14; // assignning the value
+    int  * dynamicArray = new int[4];
+    for (int i =0;i<4;i++){
+        dynamicArray[i] = i*10;
+    }
 
 
-    cout<<"New dynamic int : "<< *dynamicInt<<endl;
+    cout<<"Dynamic Array : ";
+    for(int i=0;i<4;i++){
+        cout<<dynamicArray[i]<<endl;
+    }
 
-    delete dynamicInt; // deallocating the memory
-    return 0;
+
+
+    delete[] dynamicArray;
+
+    
+
+
 }
+
+
